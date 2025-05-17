80d558036c14f217b146ae789f3ba4000e0996ad
Vlad Grigore Sima <vladgrigoresima10@gmail.com>
2025-05-17 13:11:25 +0300

    Initial Commit

---
+++ Tic Tac Toe/Tic_Tac_Toe.h
@@ -0,0 +1,18 @@
+//
+//  Tic_Tac_Toe.h
+//  Tic Tac Toe
+//
+//  Created by Vlad Grigore Sima on 17.05.2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Tic_Tac_Toe.
+FOUNDATION_EXPORT double Tic_Tac_ToeVersionNumber;
+
+//! Project version string for Tic_Tac_Toe.
+FOUNDATION_EXPORT const unsigned char Tic_Tac_ToeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tic_Tac_Toe/PublicHeader.h>
+
+
