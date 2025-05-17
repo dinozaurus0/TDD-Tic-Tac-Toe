ef4471eb0b198780255b828baed8baf8aded236c
Vlad Grigore Sima <vladgrigoresima10@gmail.com>
2025-05-17 14:45:42 +0300

    Setup & Initial Tests
    Description of the overall structure & breaking behaviour into smaller steps.
    
    Discussed the existence of 2 entities: `Field` & `Player`.
    `Field` is responsible of holding one value.
    `Player` is responsible of changing the field's value.
    
    Field:
    - be able to initialised
    - be able to hold a value
    - be able to hold an non empty value
    
    Player:
    - be able to initialised
    - be able to set a field's value property

---
+++ Tic Tac ToeTests/FieldTests.swift
@@ -0,0 +1,93 @@
+import Testing
+@testable import Tic_Tac_Toe
+
+/*
+ The rules of the tic tac toe game are the following:
+
+ a game is over when all fields are taken
+ a game is over when all fields in a column are taken by a player
+ a game is over when all fields in a row are taken by a player
+ a game is over when all fields in a diagonal are taken by a player
+ a player can take a field if not already taken
+ players take turns taking fields until the game is over
+ there are two player in the game (X and O)
+ */
+
+/*
+ 1. One field where user can introduce either X or O
+ 1.1. Creating a field should be an action on its own.
+ 1.2. A field can have a value/contain a value.
+ 1.2.1. Make value a concrete type.
+ 1.3. One player can introduce a value. At this stage consider only one.
+ 1.3.1. Define a player
+ 1.3.2. Define the value ~> Not needed at this stage
+ 1.3.3. Player can set a value
+ 1.3.4. Should Field be a value or reference type?
+ 1.4. Decide when the user wins a round ~> for this particular case would be only that he introduces a valid value.
+ */
+
+
+// Red - Green - Refactor ^_^
+
+// Least amount of the step!
+
+// action that triggers -- when it gets trigger -- what the result of this is
+
+class Field {
+    var value: Any? = nil
+}
+
+@Suite
+struct FieldTests {
+    @Test
+    func onInit_whenDefault_subjectIsNotNil() {
+        let sut = Field()
+        #expect(sut != nil)
+    }
+    
+    @Test
+    func onInit_whenDefault_assignsValueAsNil() {
+        let sut = Field()
+        
+        let value = sut.value
+        
+        #expect(value == nil)
+    }
+    
+    @Test
+    func onAssign_whenX_assignedValueIsNotNil() {
+        let sut = Field()
+        sut.value = "X"
+        
+        let value = sut.value
+        
+        #expect(value != nil)
+    }
+}
+
+struct Player {
+    func set(field: Field) {
+        field.value = "X"
+    }
+}
+
+@Suite
+struct PlayerTests {
+    @Test
+    func onInit_whenDefault_subjectIsNotNil() {
+        let sut = Player()
+        #expect(sut != nil)
+    }
+    
+    @Test
+    func onSet_whenField_fieldValueIsNotNil() {
+        let sut = Player()
+        let field = Field()
+        
+        sut.set(field: field)
+        
+        #expect(field.value != nil)
+    }
+}
+
+// :thumbs up
