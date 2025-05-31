2c79ed336fd4b35ed798960b57a93a0db1401492
Vlad Grigore Sima <vladgrigoresima10@gmail.com>
2025-05-31 14:07:07 +0300

    Make `Player` conform to `Equatable` in order to have player comparison for other tests.

--- Tic Tac ToeTests/FieldTests.swift
+++ Tic Tac ToeTests/FieldTests.swift
@@ -24,6 +24,8 @@ import Testing
  1.3.3. Player can set a value
  1.3.4. Should Field be a value or reference type?
  A field has been set by a given player?
+ - Need a way to identify which player is which
+    - How to determine that 2 players are equal?
  
  
  1.4. Decide when the user wins a round ~> for this particular case would be only that he introduces a valid value.
@@ -64,7 +66,7 @@ struct FieldTests {
     }
 }
 
-struct Player {
+struct Player: Equatable {
     func set(field: Field) {
         field.player = self
     }
@@ -80,5 +82,11 @@ struct PlayerTests {
         sut.set(field: field)
         
         #expect(field.player != nil)
+    }
+    
+    @Test
+    func equatable_whenSamePlayer_returnsTrue() {
+        let sut = Player()
+        #expect(sut == sut)
     }
 }
